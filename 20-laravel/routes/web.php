18ed775aa26824e1bfea3398e3d41ecdaf0c8c46
JuanBite <quinchiaj73@gmail.com>
2025-11-11 17:39:23 -0500

    Laravel Login

--- 20-laravel/routes/web.php
+++ 20-laravel/routes/web.php
@@ -2,6 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Pet;
+
+Route::get('/', function () {
+    // return "This is a Route: 😊";
+    return view('welcome');
+});
+
+Route::get('Hello', function () {
+    return "<h1>Hello folks, have a nice day 😁</h1>";
+});
+
+Route::get('Hello/{name}', function () {
+    return "<h1>Hello " . request()->name . "</h1>";
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +29,49 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('show/users', function() {
+    $users = App\Models\User::all();
+    dd($users->toArray());
+});
+Route::get('challenge', function () {
+    $users = \App\Models\User::take(20)->get();
+    
+    $html = '<table border="1" style="border-collapse: collapse; width: 50%; justify-content: center; margin: auto;">
+        <tr style="background-color: #f2f2f2;">
+            <th>ID</th>
+            <th>Photo</th>
+            <th>Full Name</th>
+            <th>Age</th>
+            <th>Created</th>
+        </tr>';
+    
+    foreach ($users as $user) {
+        $html .= '<tr>
+            <td style="text-align: center;">' . $user->id . '</td>
+            <td style="text-align: center;"><img src="' . $user->photo . '" width="50"></td>
+            <td style="padding: 10px;">' . $user->fullname . '</td>
+            <td style="text-align: center;">' . \Carbon\Carbon::parse($user->birthdate)->age . '</td>
+            <td style="padding: 10px;">' . $user->created_at->diffForHumans() . '</td>
+        </tr>';
+    }
+    
+    $html .= '</table>';
+    
+    return $html;
+});
+Route::get('show/pets', function() {
+    $pets = App\Models\Pet::all();
+    dd($pets->toArray()); // Dump and Die
+});
+Route::get('view/pet/{id}', function($id) {
+    $pets = App\Models\Pet::find($id);
+    return view('view-pet')->with('pet', $pets);
+    // dd($pets->toArray()); // Buscar por ID
+});
+Route::get('view/pets', function() {
+    $pets = Pet::paginate(10);
+    return view('view-pets')->with('pets', $pets);
 });
 
 require __DIR__.'/auth.php';
