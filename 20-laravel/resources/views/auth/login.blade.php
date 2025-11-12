683e95ad092242f2c381ecf3d15317c69c39bb5f
JuanBite <quinchiaj73@gmail.com>
2025-11-12 17:29:58 -0500

    Register y Login

--- 20-laravel/resources/views/auth/login.blade.php
+++ 20-laravel/resources/views/auth/login.blade.php
@@ -55,18 +55,18 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="size-10" fill="currentColor" viewBox="0 0 256 256"><path d="M208,80H96V56a32,32,0,0,1,32-32c15.37,0,29.2,11,32.16,25.59a8,8,0,0,0,15.68-3.18C171.32,24.15,151.2,8,128,8A48.05,48.05,0,0,0,80,56V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80Zm0,128H48V96H208V208Zm-80-96a28,28,0,0,0-8,54.83V184a8,8,0,0,0,16,0V166.83A28,28,0,0,0,128,112Zm0,40a12,12,0,1,1,12-12A12,12,0,0,1,128,152Z"></path></svg>
     </h1>
     <div class="card w-full max-w-sm">
-            <form method="POST" action="{{ route('login') }}" class="card-body">
+            <form method="POST" action="{{ route('login') }}" class="card-body border-t-[1px]">
                 @csrf
-                <label class="label">Email</label>
+                <label class="label text-white">Email</label>
                 <input type="text" class="input bg-[#0006]" name="email" placeholder="Enter your Email" value="{{ old('email') }}" />
                 @error('email')
-                    <small class="text-error text-sm mt-1">{{ $message }}</small>
+                    <small class="badge badge-outline badge-error w-full h-full">{{ $message }}</small>
                 @enderror
 
-                <label class="label">Password</label>
+                <label class="label text-white">Password</label>
                 <input type="password" class="input bg-[#0006]" name="password" placeholder="Enter your password" />
                 @error('password')
-                    <small class="text-error text-sm mt-1">{{ $message }}</small>
+                    <small class="badge badge-outline badge-error w-full">{{ $message }}</small>
                 @enderror
 
                 <button class="btn btn-accent hover:bg-[#a8f5b666] hover:text-white mt-4">Login</button>
@@ -75,6 +75,11 @@
                     Don’t have an account?
                     <a href="{{ route('register') }}" class="link link-default">
                         Sign up
+                    </a>
+                </p>
+                <p class="text-sm text-center mt-4">
+                    <a href="{{ route('password.request') }}" class="link link-default">
+                        Forgot your password?
                     </a>
                 </p>
             </form>
